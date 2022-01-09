6e817a59b6b3b36911f79904cdf4db3f530f6a4f
EliasNahmad <97400403+EliasNahmad@users.noreply.github.com>
2022-01-09 11:53:57 -0600

    Add files via upload

---
+++ ciclos.py
@@ -0,0 +1,14 @@
+#Fibonacci
+num0=0
+num1=1
+while num0 < 1000:
+    print("Num0= ",num0)
+    num0=num0+num1
+    num1=num0+1
+''' print("Num0= ",num0)
+    print("Num1 = ",num1)
+'''
+#For tupla
+Tup1=(0,1,2,3,4,5,6,7,8,9)
+for recorre in Tup1:
+    print(recorre)
