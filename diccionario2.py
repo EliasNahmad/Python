afdaec8971d7c627d2abcb40c23c5ec531baa6ad
EliasNahmad <97400403+EliasNahmad@users.noreply.github.com>
2022-01-09 11:33:04 -0600

    Add files via upload

---
+++ diccionario2.py
@@ -0,0 +1,17 @@
+
+dir = {"Elias":"123446"}
+print(dir)
+dir["moises"]="2468"
+print(dir)
+dir["sofia"]="135"
+print(dir)
+dir["regina"]="369"
+print(dir)
+dir["pamela"]="1357"
+print(dir)
+dir["marcela"]="987654"
+print(dir)
+print(dir["Elias"])
+nombre = dir.pop("Elias")
+print(nombre)
+print(dir)
